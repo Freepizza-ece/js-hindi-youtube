c9730f48670885220858983c4fc51e839578d00f
Freepizza-ece <2025ugec076@nitjsr.ac.in>
2026-08-08 10:36:43 +0000

    learned data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail="Yash88@gmail.com"
+var accountPassword="1234"
+accountCity="Ranchi"
+
+let accountState;
+
+
+// accountId=2
+
+console.log(accountId);
+
+//please donot use var ever
+// because of issue in block scope and functional scope
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strrict";//treat all JS code as newer version
+
+//node.js does not have the browser alert funciton 
+// alert("hello");
+
+//alert(3+3)//we are using node.js , not browser
+
+// console.log(3+3);
+console.log("Yash");
+
+
+let name="yash";
+let age=13;
+let isLoggedIn=false;
+let state=null;
+
+
+//number=>2 to the power 53 is the range of number 
+//bigint data type is used for larger number 
+//string ==>
+//boolean = false/true;
+//null (this is a data type as well as a stand alone value)(this is a stand alone value)
+//undefined==>(this means abhi value assign nhi hui hai)
+//symbol==>when ever we talk about unique val
+//object
+
+
+console.log(typeof null);
+// null is an object(null is a type of object);
+// undefined(type)==>undefined;
+
+
